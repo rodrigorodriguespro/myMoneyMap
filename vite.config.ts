fb650a6af4473f4754c9decebae89c7fefc1b8a1
Rodrigo Rodrigues <prorodrigorodrigues@gmail.com>
2024-12-28 15:18:49 -0300

    add Tailwind CSS setup with PostCSS and utility functions

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,20 @@ import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
 import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-  plugins: [inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }), react(), adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] })],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [
+    inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.tsx' } }),
+    react(),
+    adonisjs({ entrypoints: ['inertia/app/app.tsx'], reload: ['resources/views/**/*.edge'] }),
+  ],
 
   /**
    * Define aliases for importing modules from
@@ -14,6 +25,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '~/': `${getDirname(import.meta.url)}/inertia/`,
+      '#': `${getDirname(import.meta.url)}/`,
+      '@': `${getDirname(import.meta.url)}/inertia/lib`,
     },
   },
 })
